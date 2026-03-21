5ffa200eeea31d630705c97ae3641d6799b03781
elliot! <benjikrolfanpageuwu@gmail.com>
2026-03-21 18:02:52 +0000

    Inital Commit

---
+++ Behaviours/ErrorManager.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Linq;
+using UnityEngine;
+using MelonLoader.Utils;
+
+namespace GorillaAlbums.Behaviours
+{
+    public static class ErrorManager
+    {
+        public static void CheckShelf(GameObject shelf)
+        {
+            string rootFolder = Path.Combine(MelonEnvironment.ModsDirectory, "AlbumCovers");
+
+            for (int i = 1; i <= 4; i++)
+            {
+                Transform record = shelf.transform.FindDeepChild($"Record{i}");
+                if (record == null) continue;
+
+                string folder = Path.Combine(rootFolder, $"Album{i}");
+                Transform errorGO = shelf.transform.FindDeepChild("Error");
+
+                var images = Directory.GetFiles(folder, "*.*")
+                    .Where(f => f.EndsWith(".png", System.StringComparison.OrdinalIgnoreCase)
+                             || f.EndsWith(".jpg", System.StringComparison.OrdinalIgnoreCase))
+                    .ToArray();
+
+                bool hasError = images.Length == 0;
+
+                if (errorGO != null)
+                    errorGO.gameObject.SetActive(hasError);
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,57 @@
+﻿using System.Threading.Tasks;
+using MelonLoader;
+using GorillaAlbums.Tools;
+using GorillaAlbums.Behaviours;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+[assembly: MelonInfo(typeof(GorillaAlbums.Plugin), "GorillaAlbums", "1.0.5", "elligurt")]
+[assembly: MelonGame("Another Axiom", "Gorilla Tag")]
+
+namespace GorillaAlbums
+{
+    public class Plugin : MelonMod
+    {
+        private GameObject _shelfPrefab;
+        private bool _initialized;
+        
+        public override void OnInitializeMelon()
+        {
+            GorillaTagger.OnPlayerSpawned(OnPlayerSpawned);
+        }
+
+        private void OnPlayerSpawned()
+        {
+            if (_initialized) return;
+
+            _initialized = true;
+            _ = Setup();
+        }
+
+        private async Task Setup()
+        {
+            MelonLogger.Msg("[GorillaAlbums] Initializing...");
+
+            ImageManager.CreateImageFolder();
+
+            _shelfPrefab = await AssetLoader.LoadAsset<GameObject>("GorillaAlbums");
+
+            if (_shelfPrefab == null)
+            {
+                Debug.LogError("[GorillaAlbums] Failed loading shelf prefab!");
+                return;
+            }
+           
+            GameObject shelf = Object.Instantiate(_shelfPrefab);
+            shelf.SetActive(true);
+
+            shelf.transform.position = new Vector3(-64.7606f, 12.1637f, -84.5819f);
+            shelf.transform.rotation = Quaternion.Euler(0f, 271.3724f, 0f);
+
+            ErrorManager.CheckShelf(shelf);
+            ImageManager.ApplyImages(shelf);
+
+            MelonLogger.Msg("[GorillaAlbums] Setup complete.");
+        }
+    }
+}
